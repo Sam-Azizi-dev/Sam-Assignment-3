f28ed5860a8c36509d0a1cdee201863385ca3def
Sam Azizi <sadeghazizi.ca@gmail.com>
2022-12-18 11:04:02 -0800

    First Push
    get data from file and store to array of users

---
+++ src/com/coderscampus/Users.java
@@ -0,0 +1,55 @@
+package com.coderscampus;
+
+public class Users {
+	private String username;
+	private String password;
+	private String name;
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [username=" + username + ", password=" + password + ", name=" + name + "]";
+	}
+
+}
